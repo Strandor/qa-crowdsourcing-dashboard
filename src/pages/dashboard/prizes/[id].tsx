8a17d49a8f477d5ae57e9bf035638b87134dafc4
Brekig <42963322+Brekig@users.noreply.github.com>
2022-02-13 12:53:51 +0000

    cwt

--- src/pages/dashboard/prizes/[id].tsx
+++ src/pages/dashboard/prizes/[id].tsx
@@ -4,8 +4,6 @@ const Prize = () => {
 	return (
 		<Components.Layouts.Sidebar>
 			<h1>Vinningar</h1>
-			<h2>Vinningar</h2>
-			<h2>Fyrrum vinningshafar</h2>
 		</Components.Layouts.Sidebar>
 	);
 };
