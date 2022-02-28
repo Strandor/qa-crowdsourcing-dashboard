8c03d6789327cc0f63c25da5fbe475ce6c660b31
Brekig <42963322+Brekig@users.noreply.github.com>
2022-02-28 15:25:12 +0000

    cwt

--- src/pages/dashboard/prizes/[id].tsx
+++ src/pages/dashboard/prizes/[id].tsx
@@ -5,6 +5,7 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { number } from "prop-types";
+import styles from "./styles.module.scss";
 
 // { router }: AppProps
 const Prize = () => {
@@ -13,17 +14,6 @@ const Prize = () => {
 	const prizeCategories = useSelector(
 		(state: Redux.Reducers.StoreState) => state.prizeCategories.total.data
 	);
-	// const [category, setCategory] = useState({});
-
-	// useEffect(() => {
-	// 	const prizeCategories = useSelector(
-	// 		(state: Redux.Reducers.StoreState) => state.prizeCategories.total.data
-	// 	);
-	// 	// setCategory(prizeCategories.find((cat) => cat._id === id));
-	// }, [prizeCategories]);
-	// console.log(id);
-	// const [category, setCategory] = useState({});
-	// setCategory(prizeCategories.find((cat) => cat._id === id));
 
 	return (
 		<>
@@ -31,18 +21,19 @@ const Prize = () => {
 				<Components.Layouts.Sidebar>
 					<h1>Vinningar</h1>
 					<h1>id:{id}</h1>
-
-					{prizeCategories
-						.find((cat) => cat._id === id)
-						.prizes.map((prize) => (
-							<Components.Atoms.Items.Prize
-								_id={prize._id}
-								name={prize.name}
-								img={prize.img}
-								available={prize.available}
-								brandImg={prize.brandImg}
-							></Components.Atoms.Items.Prize>
-						))}
+					<div className={styles.prizesContainer}>
+						{prizeCategories
+							.find((cat) => cat._id === id)
+							.prizes.map((prize) => (
+								<Components.Atoms.Items.Prize
+									_id={prize._id}
+									name={prize.name}
+									img={prize.img}
+									available={prize.available}
+									brandImg={prize.brandImg}
+								></Components.Atoms.Items.Prize>
+							))}
+					</div>
 				</Components.Layouts.Sidebar>
 			) : (
 				<></>
